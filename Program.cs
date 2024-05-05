e490674358eeab785854a0aa80846f65694a56b1
Ernesto Abreu Peraza <ernestoabreup@gmail.com>
2024-05-05 16:47:58 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
+global using FastEndpoints;
+global using FluentValidation;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddFastEndpoints();
+
 var app = builder.Build();
+app.UseFastEndpoints();
 
 app.MapGet("/", () => "Hello World!");
 
